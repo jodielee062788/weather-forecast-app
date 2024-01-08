90850a1487677d477c5226ca042c207b5e520bc1
Jodie <jodielee062788@gmail.com>
2024-01-08 13:21:51 +1100

    Add clear history button under search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ var cityInput = document.getElementById('cityInput');
 var currentWeatherContainer = document.getElementById('currentWeather');
 var forecastContainer = document.getElementById('forecast');
 var historyList = document.getElementById('historyList');
+var clearHistoryBtn = document.getElementById('clearHistoryBtn');
 
+// Event listeners
 searchForm.addEventListener('submit', function (event) {
     event.preventDefault();
     var cityName = cityInput.value.trim();
@@ -21,75 +23,82 @@ historyList.addEventListener('click', function(event) {
     }
 });
 
+clearHistoryBtn.addEventListener('click', function() {
+    clearSearchHistory();
+});
+
+// Fetch weather data from OpenWeatherMap API
 function getWeatherData(cityName) {
-    const geocodingApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    var geocodingApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
 
     fetch(geocodingApiUrl)
         .then(response => response.json())
         .then(geocodingData => {
-            console.log('Geocoding Data:', geocodingData);
 
+            // Check for errors in the geocoding API response
             if (geocodingData.message) {
                 console.error('Geocoding API error:', geocodingData.message);
                 return Promise.reject(new Error(geocodingData.message));
             }
 
-            const { lat, lon } = geocodingData.coord;
-            const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+            // Extract latitude and longitude
+            var lat = geocodingData.coord.lat;
+            var lon = geocodingData.coord.lon;
 
+            // Construct forecast API URL
+            var forecastApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=metric';
+
+            // Fetch forecast data
             return fetch(forecastApiUrl);
         })
-        .then(response => {
-            console.log('Forecast API Response:', response);
-            return response.json();
-        })
+        .then(response => response.json())
         .then(weatherData => {
-            console.log('Weather Data:', weatherData);
 
+            // Check for errors in the forecast API response
             if (weatherData.message) {
                 console.error('Forecast API error:', weatherData.message);
                 return Promise.reject(new Error(weatherData.message));
             }
 
+            // Update current weather and forecast
             updateCurrentWeather(weatherData);
-            handleForecastData(weatherData);
+            updateForecastData(weatherData);
             addToSearchHistory(cityName);
         })
         .catch(error => {
+            // Log errors during data fetching
             console.error('Error fetching data:', error);
         });
 }
 
-
+// Update the current weather display
 function updateCurrentWeather(data) {
-    console.log('Updating Current Weather:', data);
 
+    // Check if the expected properties are present in the data
     if (data && data.list && data.list.length > 0) {
         var currentWeather = data.list[0];
 
+        // Check if essential properties are present in currentWeather
         if (currentWeather.main && currentWeather.main.temp && currentWeather.wind && currentWeather.weather && currentWeather.weather[0]) {
             var name = data.city.name;
             var main = currentWeather.main;
             var weather = currentWeather.weather[0];
             var wind = currentWeather.wind;
 
-            console.log('City Name:', name);
-            console.log('Main:', main);
-            console.log('Weather:', weather);
-            console.log('Wind:', wind);
+            // Get the weather icon URL
+            var weatherIcon = 'https://openweathermap.org/img/w/' + weather.icon + '.png';
 
-            var weatherIcon = `https://openweathermap.org/img/w/${weather.icon}.png`;
+            // Update the HTML of current weather container
+            var html = '<h2>' + name + '</h2>' +
+                '<p>Date: ' + new Date(currentWeather.dt * 1000).toLocaleDateString() + '</p>' +
+                '<img src="' + weatherIcon + '" alt="Weather Icon">' +
+                '<p>Temperature: ' + main.temp + ' °C</p>' +
+                '<p>Humidity: ' + main.humidity + '%</p>' +
+                '<p>Wind Speed: ' + wind.speed + ' m/s</p>' +
+                '<p>Weather Conditions: ' + weather.description + '</p>';
 
-            var html = `
-                <h2>${name}</h2>
-                <p>Date: ${new Date(currentWeather.dt * 1000).toLocaleDateString()}</p>
-                <img src="${weatherIcon}" alt="Weather Icon">
-                <p>Temperature: ${main.temp} °C</p>
-                <p>Humidity: ${main.humidity}%</p>
-                <p>Wind Speed: ${wind.speed} m/s</p>
-                <p>Weather Conditions: ${weather.description}</p>
-            `;
             currentWeatherContainer.innerHTML = html;
+            // Make the current weather container visible
             currentWeatherContainer.classList.remove('hidden');
         } else {
             console.error('Error: Invalid data format for current weather');
@@ -99,45 +108,44 @@ function updateCurrentWeather(data) {
     }
 }
 
-function handleForecastData(data) {
-    console.log('Forecast Data:', data);
+// update the forecast container
+function updateForecastData(data) {
 
+    // Check if the expected properties are present in the data
     if (data && data.list && data.list.length > 0) {
-        const forecastHtml = data.list.filter(item => item.dt_txt.includes('00:00:00')).map(item => {
-            const forecastDate = new Date(item.dt * 1000 + data.city.timezone * 1000); // Adjust for timezone
-            const formattedDate = forecastDate.toLocaleDateString('en-US', { timeZone: 'UTC' });
-            const weatherIcon = `https://openweathermap.org/img/w/${item.weather[0].icon}.png`;
+        var forecastHtml = data.list.filter(item => item.dt_txt.includes('00:00:00')).map(item => {
+            var forecastDate = new Date(item.dt * 1000 + data.city.timezone * 1000); // Adjust for timezone
+            var formattedDate = forecastDate.toLocaleDateString('en-US', { timeZone: 'UTC' });
+            var weatherIcon = 'https://openweathermap.org/img/w/' + item.weather[0].icon + '.png';
 
             return generateForecastCard(item, formattedDate, weatherIcon);
         }).join('');
 
+        // Update the HTML of forecast container
         forecastContainer.innerHTML = forecastHtml;
+        // Make the forecast container visible
         forecastContainer.classList.remove('hidden');
 
-        console.log('Forecast container updated and made visible.');
-        console.log('forecastContainer:', forecastContainer);
     } else {
         console.error('Error: Invalid data format for forecast');
     }
 }
 
-
+// Generate HTML for a forecast card
 function generateForecastCard(item, formattedDate, weatherIcon) {
-    return `
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">${formattedDate}</h5>
-                <img src="${weatherIcon}" alt="Weather Icon">
-                <p class="card-text">Current Temperature: ${item.main.temp} °C</p>
-                <p class="card-text">Humidity: ${item.main.humidity}%</p>
-                <p class="card-text">Wind Speed: ${item.wind.speed} m/s</p>
-                <p class="card-text">Weather Conditions: ${item.weather[0].description}</p>
-            </div>
-        </div>
-    `;
+    return '<div class="card">' +
+        '<div class="card-body">' +
+        '<h5 class="card-title">' + formattedDate + '</h5>' +
+        '<img src="' + weatherIcon + '" alt="Weather Icon">' +
+        '<p class="card-text">Current Temperature: ' + item.main.temp + ' °C</p>' +
+        '<p class="card-text">Humidity: ' + item.main.humidity + '%</p>' +
+        '<p class="card-text">Wind Speed: ' + item.wind.speed + ' m/s</p>' +
+        '<p class="card-text">Weather Conditions: ' + item.weather[0].description + '</p>' +
+        '</div>' +
+        '</div>';
 }
 
-
+// Update search history in the UI
 function updateSearchHistory(cityName) {
     var listItem = document.createElement('li');
     listItem.classList.add('list-group-item');
@@ -145,6 +153,7 @@ function updateSearchHistory(cityName) {
     historyList.appendChild(listItem);
 }
 
+// Add a city to the search history
 function addToSearchHistory(cityName) {
     var history = loadSearchHistory();
     if (!history.includes(cityName)) {
@@ -154,20 +163,32 @@ function addToSearchHistory(cityName) {
     }
 }
 
+// Load search history from local storage
 function loadSearchHistory() {
     var history = JSON.parse(localStorage.getItem('searchHistory')) || [];
     historyList.innerHTML = '';
-    history.forEach(city => updateSearchHistory(city));
+    
+    // Check if there are items in the search history
+    if (history.length > 0) {
+        history.forEach(city => updateSearchHistory(city));
+        clearHistoryBtn.style.display = 'block'; // Show the "Clear History" button
+    } else {
+        clearHistoryBtn.style.display = 'none'; // Hide the "Clear History" button
+    }
+    
     return history;
 }
 
+// Save search history to local storage
 function saveSearchHistory(history) {
     localStorage.setItem('searchHistory', JSON.stringify(history));
 }
 
+// Clear search history
 function clearSearchHistory() {
     localStorage.removeItem('searchHistory');
     historyList.innerHTML = '';
 }
 
+// Load search history on page load
 loadSearchHistory();
